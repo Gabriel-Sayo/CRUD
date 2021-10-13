7667e63acc6cca2fcc99f431857563068f5bf9e3
Gabriel-Sayo <72852759+Gabriel-Sayo@users.noreply.github.com>
2021-10-13 17:48:28 +0800

    Add files via upload

---
+++ error.php
@@ -0,0 +1,30 @@
+<?php include("config.php");?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="fontawesome/css/all.css" rel="stylesheet">
+    <title>Sample CRUD</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 offset-md-1">
+            <h2 class="mt-5 mb-3">Invalid Request</h2>
+            <div class="alert alert-danger">
+              System error! Please Go <a href="index.php">Back</a> and contact the administrator.
+            </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+  </body>
+</html>
